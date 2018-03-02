f08cd7f890cfa9482aea9fc83d7b9425f3aa5642
VivianSolide <vivian.sarazin@gmail.com>
2018-03-02 20:02:19 +0100

    inline handlers

--- client/src/views/QuizzMusic.vue
+++ client/src/views/QuizzMusic.vue
@@ -11,6 +11,7 @@
             </b-field>
         </form>
     </div>
+    <div class="container">
     <div class="columns is-multiline">
       <div class="column is-one-quarter" v-for="artist in artists">
         <div class="card">
@@ -19,8 +20,12 @@
               <img v-if="artist.images[0]" v-bind:src=artist.images[0].url alt="Placeholder image">
               <img v-else src="https://placehold.it/600x600" alt="Placeholder image">
             </figure>
+            <a v-bind:href=artist.external_urls.spotify>
+            <button class="button">Écouter</button>
+            </a>
           </div>
-          <button class="button" @click.prevent="sendArtist" >{{ artist.name }} </button>
+          <button class="button is-primary" @click.prevent="sendArtist(artist.name)">{{ artist.name }} </button>
+        </div>
         </div>
       </div>
       </div>  
@@ -36,21 +41,35 @@ import api from '../api';
       return {
         search: "",
         artists: [],
+        artist: "",
       }
     },
     methods: {
       getMusic() {
-        api.getMusic(this.search)
+        api
+        .getMusic(this.search)
         .then((artists) => {
           this.artists = artists.body.artists.items
-          console.log(this.artists)
         })
         .catch(err => {
           this.error = err;
         });
       },
-      sendArtist() {
-        console.log(this.artist)
+
+
+      sendArtist(message) {
+        console.log(message)
+        api
+        .sendArtist({
+          id: '5a990ebea0552c1dfd2a13e4',
+          artist: this.artist
+        })
+        .then((data) => {
+          // this.$router.push('/quizz-info');
+          })
+          .catch(err => {
+          this.error = err;
+        });
       }
     }
 }
