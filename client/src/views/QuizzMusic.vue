34e54a181f3dbd80be44d8dcc84e5ec8527ea5e4
VivianSolide <vivian.sarazin@gmail.com>
2018-03-02 17:08:02 +0100

    spotify list

--- client/src/views/QuizzMusic.vue
+++ client/src/views/QuizzMusic.vue
@@ -1,24 +1,24 @@
 <template>
 <div>
-<div class="container">
-  <img src="https://lh3.googleusercontent.com/D_04_AQH-II9TyLU26GDtFxPZohYWbl-SGKb1msbi5XlIGMAEr0HI01RvZ7Afi5BzQ=w300">
-  <h2>Quel est ton groupe préféré ?</h2>
-  <form>
-  <b-field>
-    <b-input 
-        placeholder="Search..."
-        v-model="search"
-        type="search"
-        icon="magnify">
-    </b-input>
-  </b-field>
-  <button class="button" @click.prevent="getMusic">Submit</button>
-  </form>
-</div>
-<div>
-  <pre>{{ data }}</pre>
-</div>  
-</div>  
+    <div class="container">
+        <img src="https://lh3.googleusercontent.com/D_04_AQH-II9TyLU26GDtFxPZohYWbl-SGKb1msbi5XlIGMAEr0HI01RvZ7Afi5BzQ=w300">
+        <h2>Quel est ton groupe préféré ?</h2>
+        <form>
+            <b-field>
+                <b-input placeholder="Search..." v-model="search" type="search" icon="magnify">
+                </b-input>
+                <button class="button" @click.prevent="getMusic">Submit</button>
+            </b-field>
+        </form>
+    </div>
+    <div>
+      <ul>
+        <li v-for="artist in artists">
+           {{ artist.name }}
+        </li>  
+      </ul>
+    </div>  
+</div>    
 </template>
 
 <script>
@@ -28,18 +28,14 @@ import api from '../api';
     data () {
       return {
         search: "",
-        data: ""
+        artists: [],
       }
     },
     methods: {
       getMusic() {
         api.getMusic(this.search)
-        .then((data) => {
-          // console.log(data.body.artists.items)
-          this.data = data.body.artists.items
-          console.log(this.data)
-          
-          // this.$router.push('/about');
+        .then((artists) => {
+          this.artists = artists.body.artists.items
         })
         .catch(err => {
           this.error = err;
