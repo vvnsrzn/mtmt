d0343b86a15801c14139891ae8aaceb29636a9b9
VivianSolide <vivian.sarazin@gmail.com>
2018-03-02 10:53:41 +0100

    quizzmusic

--- client/src/components/QuizzQuestion.vue
+++ client/src/components/QuizzQuestion.vue
@@ -1,7 +1,13 @@
 <template>
-<div>
+<div class="container">
   <img src="https://lh3.googleusercontent.com/D_04_AQH-II9TyLU26GDtFxPZohYWbl-SGKb1msbi5XlIGMAEr0HI01RvZ7Afi5BzQ=w300">
   <p>Music</p>
+  <b-field>
+    <b-input placeholder="Search..."
+                type="search"
+                icon="magnify">
+            </b-input>
+        </b-field>
 </div>
 </template>
 
